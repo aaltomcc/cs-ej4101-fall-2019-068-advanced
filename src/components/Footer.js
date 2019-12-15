29e1fc7da817f373f696c49065b73ae89318eaa1
tterimaa <tterimaa@gmail.com>
2019-12-15 13:26:34 +0200

    Edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from "react"
+import Image from "./image"
 
 export const Footer = () => {
   return (
     <div className="footerWrapper">
       <footer>
         <div>
+            <div>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+              <Image />
+            </div>
+            </div>
           <p>
             © Tarmo Terimaa {new Date().getFullYear()}, Built with{" "}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Projects from "../components/Projects"
 import About from "../components/About"
@@ -11,12 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <About />
     <Projects />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
   </Layout>
 )
 
